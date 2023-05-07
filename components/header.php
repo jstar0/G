457c4d5c86cab798b2a94d78f81f4aec46494ffc
King Star <mcxin.y@gmail.com>
2023-05-07 17:21:53 +0800

    Slightly change the order of the top bar.

--- components/header.php
+++ components/header.php
@@ -113,10 +113,10 @@ if (isset($_POST['DYLM'])) {
                         <?php endif; ?>
                     <?php endwhile; ?> -->
                     
+                    <a href="https://forum.mcxin.top" target="_blank">论坛</a>
                     <a href="https://map.mcxin.top" target="_blank">地图</a>
                     <a href="https://status.mcxin.top" target="_blank">状态</a>
                     <a href="https://pan.mcxin.top" target="_blank">网盘</a>
-                    <a href="https://forum.mcxin.top" target="_blank">论坛</a>
                     <a href="https://account.mcxin.top" target="_blank">账号平台</a>
                     
                     <?php if ($this->options->enableHeaderSearch): ?>
